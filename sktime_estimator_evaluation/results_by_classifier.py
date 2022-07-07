25e6544ecd818a2f12398bd341d2a27c7cbb5297
Tony Bagnall <ajb@uea.ac.uk>
2022-07-07 13:06:20 +0100

    results by classifier

---
+++ sktime_estimator_evaluation/results_by_classifier.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+import csv
+from root import ROOT_DIR
+
+def get_classifier_results(classifier, root =ROOT_DIR, package = "tsml"):
+    """Load the results for a single classifier.
+
+     Load from results into a dictionary of {problem_names: accuracy (numpy array)
+    """
+    print(root)
+    cls_file = "results/"+package+"/ByClassifier/"+classifier+"_TESTFOLDS.csv"
+    print(cls_file)
+    abspath = os.path.join(root, cls_file)
+    file = open(abspath, "r", encoding="utf-8")
+    #Read and discard header line
+    #Look for results file locally
+
+    #Look on tsc.com?
+
+    #load from CSV
+
+    #return array
+
+
+get_classifier_results("Arsenal")
