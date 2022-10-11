03e0db9e48713841f4dfa6604b1303beb33a6722
Tony Bagnall <ajb@uea.ac.uk>
2022-10-11 14:27:43 +0100

    move estimator lists to classification_experiments.py

--- sktime_estimator_evaluation/experiments/classification_experiments.py
+++ sktime_estimator_evaluation/experiments/classification_experiments.py
@@ -25,12 +25,48 @@ from sktime.datasets import load_from_tsfile_to_dataframe as load_ts
 """Prototype mechanism for testing classifiers on the UCR format. This mirrors the
 mechanism used in Java,
 https://github.com/TonyBagnall/uea-tsc/tree/master/src/main/java/experiments
-but isfrom sktime.classification.interval_based import (
-    CanonicalIntervalForest,
- not yet as engineered. However, if you generate results using the method
+but is not yet as engineered. However, if you generate results using the method
 recommended here, they can be directly and automatically compared to the results
 generated in java.
 """
+
+
+from sktime.registry import all_estimators
+
+
+def list_all_multivariate_capable_classifiers():
+    """ Return a list of all multivariate capable classifiers in sktime."""
+    cls = []
+    from sktime.registry import all_estimators
+    cls = all_estimators(estimator_types="classifier",
+                         filter_tags={"capability:multivariate":True}
+                         )
+    names = [i for i, _ in cls]
+    return names
+
+
+def list_estimators(estimator_type="classifier", multivariate_only=False,
+                    univariate_only=False,
+                    dictionary=True):
+    """ Return a list of all estimators of given type in sktime."""
+    cls = []
+    filter_tags = {}
+    if multivariate_only:
+        filter_tags["capability:multivariate"] = True
+    if univariate_only:
+        filter_tags["capability:multivariate"] = False
+    cls = all_estimators(estimator_types=estimator_type, filter_tags=filter_tags)
+    names= [i for i, _ in cls]
+    return names
+
+
+str=list_estimators(estimator_type="classifier")
+for s in str:
+    print(f"\"{s}\",")
+
+str=list_estimators(estimator_type="clusterer")
+for s in str:
+    print(f"\"{s}\",")
 
 
 def demo_loading():
