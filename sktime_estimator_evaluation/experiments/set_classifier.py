50260b57ce983b7fc92cd4f9f557e7a10ac5f829
Tony Bagnall <ajb@uea.ac.uk>
2022-10-17 12:57:08 +0100

    add dummy classifier to set classifiers

--- sktime_estimator_evaluation/experiments/set_classifier.py
+++ sktime_estimator_evaluation/experiments/set_classifier.py
@@ -5,7 +5,7 @@ __author__ = ["TonyBagnall"]
 from sklearn.ensemble import RandomForestClassifier
 
 from sktime.classification.deep_learning import CNNClassifier
-#from sktime.classification.dummy import DummyClassifier
+from sktime.classification.dummy import DummyClassifier
 from sktime.classification.dictionary_based import (
     MUSE,
     WEASEL,
@@ -211,6 +211,8 @@ def set_classifier(cls, resample_id=None, train_file=False, n_jobs=1):
             random_state=resample_id,
             save_transformed_data=train_file,
         )
+    elif name == "dummy" or name == "dummyclassifier":
+        return DummyClassifier()
     elif name == "composabletimeseriesforestclassifier":
         return ComposableTimeSeriesForestClassifier()
 #    elif name == "dummy" or name == "dummyclassifier":
