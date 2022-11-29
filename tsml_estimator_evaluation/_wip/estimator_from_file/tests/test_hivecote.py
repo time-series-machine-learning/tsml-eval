b9f887326c812206aaece7e1019b3b206247b5ab
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2022-11-29 13:59:05 +0000

    file hc test

--- tsml_estimator_evaluation/_wip/estimator_from_file/tests/test_hivecote.py
+++ tsml_estimator_evaluation/_wip/estimator_from_file/tests/test_hivecote.py
@@ -27,7 +27,31 @@ def test_hivecote_from_file():
     hc2.fit(train_X, train_y)
     probas = hc2.predict_proba(test_X)
 
-    assert _assert_array_almost_equal(probas[0], np.array([0.5, 0.5]), decimal=4)
+    # Component weights (a=4):
+    # TDE: 0.941621858
+    # STC: 0.88583781517
+    # DrCIF: 0.8325698605
+    # Arsenal: 0.8325698605
+
+    # First case test probabilities:
+    # TDE: 0.2091581219241968, 0.7908418780758033
+    # STC: 0.025, 0.975
+    # DrCIF: 0.066, 0.934
+    # Arsenal: 0.0, 1.0
+
+    # Weighted probabilities:
+    # TDE: 0.19694785938, 0.74467399861
+    # STC: 0.02214594537, 0.86369186979
+    # DrCIF: 0.05494961079 , 0.7776202497
+    # Arsenal: 0.0, 0.8325698605
+
+    # Sum of weighted probabilities:
+    # 0.27404341554, 3.2185559786
+
+    # Normalised weighted probabilities:
+    # 0.07846402768, 0.92153597231
+
+    assert _assert_array_almost_equal(probas[0], np.array([0.0785, 0.9215]), decimal=4)
 
 
 def test_hivecote_from_file_check_estimator():
