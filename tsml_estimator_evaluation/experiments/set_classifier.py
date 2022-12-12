dbe5e407739a839d60992482e35452d5cfd0a9a8
Tony Bagnall <ajb@uea.ac.uk>
2022-12-12 15:47:03 +0000

    switch to HC2 EigenWorms

--- tsml_estimator_evaluation/experiments/set_classifier.py
+++ tsml_estimator_evaluation/experiments/set_classifier.py
@@ -190,8 +190,7 @@ def set_classifier(cls, resample_id=None, train_file=False, n_jobs=1, contract=0
         return HIVECOTEV1(random_state=resample_id)
     elif name == "hc2" or name == "hivecotev2":
         from sktime.classification.hybrid import HIVECOTEV2
-
-        return HIVECOTEV2(random_state=resample_id)
+        return HIVECOTEV2(random_state=resample_id, n_jobs=n_jobs)
     # Interval based
     elif name == "rise-500":
         from sktime.classification.interval_based import RandomIntervalSpectralEnsemble
