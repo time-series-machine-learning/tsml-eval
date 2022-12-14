37010b649b88d3bcfafeb4e1d393fbc9efb912a1
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2022-12-14 11:59:21 +0000

    extensions and theme change

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """Configuration file for the Sphinx documentation builder."""
 
+import os
+import sys
+
+import tsml_estimator_evaluation
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+ON_READTHEDOCS = os.environ.get("READTHEDOCS") == "True"
+if not ON_READTHEDOCS:
+    sys.path.insert(0, os.path.abspath(".."))
+RTD_VERSION = os.environ.get("READTHEDOCS_VERSION", "local")
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -10,17 +23,21 @@
 project = "tsml-estimator-evaluation"
 copyright = "2022, Matthew Middlehurst"
 author = "Matthew Middlehurst"
-release = "0.0.1"
+version = tsml_estimator_evaluation.__version__
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
+    "sphinx_gallery.load_style",
+    "numpydoc",
+    "nbsphinx",
+    "sphinx_design",
 ]
 
 templates_path = ["_templates"]
@@ -30,5 +47,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "nature"
 html_static_path = ["_static"]
