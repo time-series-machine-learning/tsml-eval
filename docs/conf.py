e997cbb4bdc6115d479f0ad2781155e7f0b25e44
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2022-12-14 12:19:51 +0000

    doc dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -42,11 +42,11 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", ".ipynb_checkpoints", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "nature"
+html_theme = "furo"
 html_static_path = ["_static"]
