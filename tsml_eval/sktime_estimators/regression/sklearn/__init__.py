66a8fe23288f8550fd894946176d3d3f2079bd4b
David Guijo Rubio <47889499+dguijo@users.noreply.github.com>
2022-12-16 11:56:05 +0000

    Standard classifiers david (#30)
    * madapting the local run code, brnch first commit
    
    * sklearn regressor added
    
    * model params
    
    * sklearn regressors ready
    
    * Minor changes

--- tsml_eval/sktime_estimators/regression/sklearn/__init__.py
+++ tsml_eval/sktime_estimators/regression/sklearn/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """"""
-__all__ = ["RotationForest"]
+__all__ = ["RotationForest", "SklearnBaseRegressor"]
 
 from tsml_eval.sktime_estimators.regression.sklearn.rotation_forest import (
     RotationForest,
 )
+
+from tsml_eval.sktime_estimators.regression.sklearn.sklearn_regressor import (
+    SklearnBaseRegressor,
+)
+
