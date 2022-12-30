462ce6e61e0fa73e0b25512d2831d3ed8ec8b81d
Tony Bagnall <ajb@uea.ac.uk>
2022-12-30 12:07:00 +0000

    set tuning score to zero if only one cluster

--- tsml_eval/experiments/distance_clustering_experiments.py
+++ tsml_eval/experiments/distance_clustering_experiments.py
@@ -42,7 +42,7 @@ def tune_window(metric: str, train_X, n_clusters):
     """Tune window."""
     best_w = 0
     best_score = 0
-    for w in np.arange(0, 1, 0.01):
+    for w in np.arange(0, 1, 0.05):
         cls = TimeSeriesKMeans(
             metric=metric, distance_params={"window": w}, n_clusters=n_clusters
         )
