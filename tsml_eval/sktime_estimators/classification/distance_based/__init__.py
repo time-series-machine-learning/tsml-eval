dc6c4a7581ad7f636707811081025db6b27660d4
Tony Bagnall <ajb@uea.ac.uk>
2022-12-30 13:40:29 +0000

    formatting classification_experiments.py

--- tsml_eval/sktime_estimators/classification/distance_based/__init__.py
+++ tsml_eval/sktime_estimators/classification/distance_based/__init__.py
@@ -3,6 +3,6 @@
 __all__ = [
     "KNeighborsTimeSeriesClassifier",
 ]
-
-from tsml_eval.sktime_estimators.classification.distance_based._time_series_neighbors \
-    import KNeighborsTimeSeriesClassifier
+from tsml_eval.sktime_estimators.classification.distance_based._time_series_neighbors import (  # noqa
+    KNeighborsTimeSeriesClassifier,
+)
