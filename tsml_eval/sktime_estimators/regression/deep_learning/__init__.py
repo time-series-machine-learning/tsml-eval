fdd879563ef991dc3fc9023ed662e0598a6e50cc
Tony Bagnall <ajb@uea.ac.uk>
2023-01-05 12:46:55 +0000

    add resnet regressor

---
+++ tsml_eval/sktime_estimators/regression/deep_learning/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Deep learning based regressors."""
+__all__ = [
+    "ResNetRegressor",
+]
+
+from tsml_eval.sktime_estimators.regression.deep_learning.resnet import ResNetRegressor
---
+++ tsml_eval/sktime_estimators/regression/deep_learning/fcn.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Fully Connected Neural Network (FCNN) for regression."""
