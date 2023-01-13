7f4cfc68a2163a184f601176f37dcca46d74772b
David Guijo-Rubio <hew22kqu@uea.ac.uk>
2023-01-13 15:30:47 +0000

    fresh prince working right

---
+++ tsml_eval/sktime_estimators/regression/featured_based/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Feature based time series regressors."""
+__all__ = ["FreshPRINCERegressor"]
+
+from tsml_eval.sktime_estimators.regression.featured_based.fresh_prince import (
+    FreshPRINCERegressor,
+)
