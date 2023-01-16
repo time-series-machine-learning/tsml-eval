5d45916897973aa738fe6dec06ed409ee6dda0a1
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-01-16 13:28:36 +0000

    davies_bouldin_score_from_file

--- tsml_eval/evaluation/metrics.py
+++ tsml_eval/evaluation/metrics.py
@@ -3,10 +3,11 @@
 
 __author__ = ["MatthewMiddlehurst"]
 
-__all__ = ["clustering_accuracy"]
+__all__ = ["clustering_accuracy", "davies_bouldin_score_from_file"]
 
+import numpy as np
 from scipy.optimize import linear_sum_assignment
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, davies_bouldin_score
 
 
 def clustering_accuracy(y_true, y_pred):
@@ -15,3 +16,14 @@ def clustering_accuracy(y_true, y_pred):
     row, col = linear_sum_assignment(matrix.max() - matrix)
     s = sum([matrix[row[i], col[i]] for i in range(len(row))])
     return s / y_pred.size
+
+
+def davies_bouldin_score_from_file(X, file_path):
+    """Calculate Davies-Bouldin score from a results file."""
+    y = np.zeros(len(X))
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines[2:]):
+            y[i] = float(line.split(",")[1])
+
+    return davies_bouldin_score(X, y)
