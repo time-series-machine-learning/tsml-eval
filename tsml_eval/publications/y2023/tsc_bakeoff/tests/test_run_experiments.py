175084a9c7756d1023d3503c3c865215c10e3bdb
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-09-01 10:57:12 +0100

    test

--- tsml_eval/publications/y2023/tsc_bakeoff/tests/test_run_experiments.py
+++ tsml_eval/publications/y2023/tsc_bakeoff/tests/test_run_experiments.py
@@ -30,7 +30,7 @@ def test_run_tsc_bakeoff_experiment():
         "-ow",
     ]
 
-    _run_experiment(args, overwrite=True, predefined_resample=False)
+    _run_experiment(args, predefined_resample=False)
 
     test_file = f"{result_path}{classifier}/Predictions/{dataset}/testResample0.csv"
     assert os.path.exists(test_file)
