d828fd255be8e6c61b2fa4ccac8aa1b1e412fcf0
Matthew Middlehurst <M.Middlehurst@uea.ac.uk>
2023-10-12 16:44:09 +0100

    Distance clustering paper webpage (#125)
    * remove todo
    
    * unskip notebooks
    
    * experiment kwargs, normalisation and n_clusters
    
    * skeleton docs
    
    * kwargs tests and fixes
    
    * back to aeon 3
    
    * test threading
    
    * scaler
    
    * threaded
    
    * kwargs
    
    * RIST to rocket
    
    * results csv
    
    * temp distance page
    
    * distance webpage
    
    * coverage and fixes
    
    * fixes
    
    * bug fix plot alignment
    
    * removed extra costmatrix
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Revert "[ENH] Cluster experiment update (#124)"
    
    This reverts commit 47142b96ceb31992a2f59a3d34f2d546a5716644.
    
    ---------
    
    Co-authored-by: Chris Holder <chrisholder987@hotmail.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- tsml_eval/publications/y2023/distance_based_clustering/__init__.py
+++ tsml_eval/publications/y2023/distance_based_clustering/__init__.py
@@ -1,1 +1,15 @@
 """Files for distance-based clustering publication."""
+
+__all__ = [
+    "_set_distance_clusterer",
+    "_run_experiment",
+    "distance_based_clusterers",
+]
+
+from tsml_eval.publications.y2023.distance_based_clustering.run_distance_experiments import (  # noqa: E501
+    _run_experiment,
+)
+from tsml_eval.publications.y2023.distance_based_clustering.set_distance_clusterer import (  # noqa: E501
+    _set_distance_clusterer,
+    distance_based_clusterers,
+)
