69ecec215db80cef1d75d708936f362585f7e065
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-11-21 23:02:28 +0000

    api

--- tsml_eval/evaluation/__init__.py
+++ tsml_eval/evaluation/__init__.py
@@ -1,1 +1,31 @@
 """Results evaluation tools."""
+
+__all__ = [
+    "evaluate_classifiers",
+    "evaluate_classifiers_from_file",
+    "evaluate_classifiers_by_problem",
+    "evaluate_clusterers",
+    "evaluate_clusterers_from_file",
+    "evaluate_clusterers_by_problem",
+    "evaluate_regressors",
+    "evaluate_regressors_from_file",
+    "evaluate_regressors_by_problem",
+    "evaluate_forecasters",
+    "evaluate_forecasters_from_file",
+    "evaluate_forecasters_by_problem",
+]
+
+from tsml_eval.evaluation.multiple_estimator_evaluation import (
+    evaluate_classifiers,
+    evaluate_classifiers_by_problem,
+    evaluate_classifiers_from_file,
+    evaluate_clusterers,
+    evaluate_clusterers_by_problem,
+    evaluate_clusterers_from_file,
+    evaluate_forecasters,
+    evaluate_forecasters_by_problem,
+    evaluate_forecasters_from_file,
+    evaluate_regressors,
+    evaluate_regressors_by_problem,
+    evaluate_regressors_from_file,
+)
