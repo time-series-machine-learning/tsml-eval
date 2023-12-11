6fddcb362aaa96f441f33230ac8f4cf8a2f173fc
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-12-11 23:57:22 +0000

    hydra examples

--- tsml_eval/estimators/classification/convolution_based/hydra.py
+++ tsml_eval/estimators/classification/convolution_based/hydra.py
@@ -73,7 +73,18 @@ class HYDRA(BaseClassifier):
 
 
 class MultiRocketHydra(BaseClassifier):
-    """aeon-MultiRocketHydra classifier-adaptor."""
+    """
+    MultiRocket-Hydra Classifier
+
+    Examples
+    --------
+    >>> from tsml_eval.estimators.classification.convolution_based import MultiRocketHydra
+    >>> from tsml.datasets import load_minimal_chinatown
+    >>> X, y = load_minimal_chinatown()
+    >>> classifier = MultiRocketHydra()
+    >>> classifier.fit(X, y)
+    >>> y_pred = classifier.predict(X)
+    """
 
     _tags = {
         "capability:multithreading": False,
