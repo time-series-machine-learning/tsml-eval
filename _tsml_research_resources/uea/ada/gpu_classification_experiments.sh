20125c099cda8f6b1493bbc59ef285cd46023cd6
Matthew Middlehurst <pfm15hbu@gmail.com>
2023-12-21 17:18:47 +0000

    [ENH] Iridis experiment scripts and instructions (#162)
    * script updates
    
    * rename

--- _tsml_research_resources/uea/ada/gpu_classification_experiments.sh
+++ _tsml_research_resources/uea/ada/gpu_classification_experiments.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# CHECK:
+# CHECK before each new run:
 #   datasets (list of problems)
 #   results_dir (where to check/write results),
-#   for clusterer in (the clusterers we are running)
-
-# While reading from them is fine, please dont write anything to the default directories in this script
+#   classifiers_to_run (list of classifiers to run)
+# While reading is fine, please dont write anything to the default directories in this script
 
 # To use GPU resources you need to be given access (gpu qos), which involves emailing hpc.admin@uea.ac.uk
 # Ask Tony or on slack, and read the GPU section in https://my.uea.ac.uk/divisions/it-and-computing-services/service-catalogue/research-it-services/hpc/ada-cluster/using-ada/jobs
@@ -31,44 +30,63 @@ max_memory=90000
 # Max allowable is 7 days - 168 hours
 max_time="168:00:00"
 
-# Start point for the script i.e. 3 datasets, 3 clusterers = 9 jobs to submit, start_point=5 will skip to job 5
+# Start point for the script i.e. 3 datasets, 3 classifiers = 9 jobs to submit, start_point=5 will skip to job 5
 start_point=1
 
 # Datasets to use and directory of data files. Default is Tony's work space, all should be able to read these. Change if you want to use different data or lists
 data_dir="/gpfs/home/ajb/Data/"
-datasets="/gpfs/home/ajb/DataSetLists/TSC_112_2019.txt"
+datasets="/gpfs/home/ajb/DataSetLists/Classification.txt"
 
 # Put your home directory here
 local_path="/gpfs/home/$username/"
 
 # Results and output file write location. Change these to reflect your own file structure
-results_dir=$local_path"ClusteringResults/results/"
-out_dir=$local_path"ClusteringResults/output/"
+results_dir=$local_path"ClassificationResults/results/"
+out_dir=$local_path"ClassificationResults/output/"
 
 # The python script we are running
-script_file_path=$local_path"Code/tsml-eval/tsml_eval/experiments/clustering_experiments.py"
+script_file_path=$local_path"Code/tsml-eval/tsml_eval/experiments/classification_experiments.py"
 
 # Environment name, change accordingly, for set up, see https://hackmd.io/ds5IEK3oQAquD4c6AP2xzQ
 # Separate environments for GPU (Python 3.8) and CPU (Python 3.10) are recommended
 env_name="tsml-eval-gpu"
 
-# You can add extra arguments here. See tsml_eval/utils/experiments.py parse_args
+# You can add extra arguments here. See tsml_eval/utils/arguments.py parse_args
 # You will have to add any variable to the python call close to the bottom of the script
+# and possibly to the options handling below
 
-# generate a results file for the test data as well as train, set to empty string to stop
-generate_test_files="-te"
+# generate a results file for the train data as well as test, usually slower
+generate_train_files="true"
 
-# If set to -pr, looks for <problem><resample>_TRAIN.ts files. This is useful for running tsml-java resamples
-predefined_folds=""
+# If set for true, looks for <problem><fold>_TRAIN.ts file. This is useful for running tsml-java resamples
+predefined_folds="false"
 
-# List valid clusterers e.g KMeans KMedoids
-# See set_clusterer for aliases
+# Classifiers to loop over. Must be seperated by a space
+# See list of potential classifiers in set_classifier
+classifiers_to_run="CNNClassifier FCNClassifier"
+
+# Normalise data before fit/predict
+normalise_data="true"
+
+# ======================================================================================
+# ======================================================================================
+# Dont change anything under here (unless you want to change how the experiment
+# is working)
+# ======================================================================================
+# ======================================================================================
+
+# Set to -tr to generate test files
+generate_train_files=$([ "${generate_train_files,,}" == "true" ] && echo "-tr" || echo "")
+
+# Set to -pr to use predefined folds
+predefined_folds=$([ "${predefined_folds,,}" == "true" ] && echo "-pr" || echo "")
+
+# Set to -rn to normalise data
+normalise_data=$([ "${normalise_data,,}" == "true" ] && echo "-rn" || echo "")
+
 count=0
 while read dataset; do
-for clusterer in KMeans KMedoids
-do
-
-# Dont change anything after here for regular runs
+for classifier in $classifiers_to_run; do
 
 # Skip to the script start point
 ((count++))
@@ -83,14 +101,14 @@ do
     num_jobs=$(squeue -u ${username} --format="%20P %5t" -r | awk '{print $2, $1}' | grep -e "R ${queue}" -e "PD ${queue}" | wc -l)
 done
 
-mkdir -p ${out_dir}${clusterer}/${dataset}/
+mkdir -p ${out_dir}${classifier}/${dataset}/
 
 # This skips jobs which have test/train files already written to the results directory. Only looks for Resamples, not Folds (old file name)
 array_jobs=""
 for (( i=start_fold-1; i<max_folds; i++ ))
 do
-    if [ -f "${results_dir}${clusterer}/Predictions/${dataset}/trainResample${i}.csv" ]; then
-        if [ "${generate_test_files}" == "true" ] && ! [ -f "${results_dir}${clusterer}/Predictions/${dataset}/testResample${i}.csv" ]; then
+    if [ -f "${results_dir}${classifier}/Predictions/${dataset}/testResample${i}.csv" ]; then
+        if [ "${generate_train_files}" == "true" ] && ! [ -f "${results_dir}${classifier}/Predictions/${dataset}/trainResample${i}.csv" ]; then
             array_jobs="${array_jobs}${array_jobs:+,}$((i + 1))"
         fi
     else
@@ -109,11 +127,11 @@ echo "#!/bin/bash
 #SBATCH --mail-user=${mailto}
 #SBATCH -p ${queue}
 #SBATCH -t ${max_time}
-#SBATCH --job-name=${clusterer}${dataset}
+#SBATCH --job-name=${classifier}${dataset}
 #SBATCH --array=${array_jobs}
 #SBATCH --mem=${max_memory}M
-#SBATCH -o ${out_dir}${clusterer}/${dataset}/%A-%a.out
-#SBATCH -e ${out_dir}${clusterer}/${dataset}/%A-%a.err
+#SBATCH -o ${out_dir}${classifier}/${dataset}/%A-%a.out
+#SBATCH -e ${out_dir}${classifier}/${dataset}/%A-%a.err
 
 . /etc/profile
 
@@ -123,16 +141,16 @@ module add cudnn/7.6.5
 source activate $env_name
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/gpfs/home/${username}/.conda/envs/${env_name}/lib/
 
-# Input args to the default clustering_experiments are in main method of
-# https://github.com/time-series-machine-learning/tsml-eval/blob/main/tsml_eval/experiments/clustering_experiments.py
-python -u ${script_file_path} ${data_dir} ${results_dir} ${clusterer} ${dataset} \$((\$SLURM_ARRAY_TASK_ID - 1)) ${generate_test_files} ${predefined_folds}"  > generatedFileGPU.sub
+# Input args to the default classification_experiments are in main method of
+# https://github.com/time-series-machine-learning/tsml-eval/blob/main/tsml_eval/experiments/classification_experiments.py
+python -u ${script_file_path} ${data_dir} ${results_dir} ${classifier} ${dataset} \$((\$SLURM_ARRAY_TASK_ID - 1)) ${generate_train_files} ${predefined_folds} ${normalise_data}"  > generatedFileGPU.sub
 
-echo ${count} ${clusterer}/${dataset}
+echo ${count} ${classifier}/${dataset}
 
 sbatch < generatedFileGPU.sub
 
 else
-    echo ${count} ${clusterer}/${dataset} has finished all required resamples, skipping
+    echo ${count} ${classifier}/${dataset} has finished all required resamples, skipping
 fi
 
 fi
