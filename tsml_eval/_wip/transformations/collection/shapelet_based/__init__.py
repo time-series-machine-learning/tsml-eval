ecd1463a67a6255ae4726415d1dcdd8509449798
Tony Bagnall <ajb@uea.ac.uk>
2024-06-17 16:01:37 +0100

    clone shapelet

---
+++ tsml_eval/_wip/transformations/collection/shapelet_based/__init__.py
@@ -0,0 +1,7 @@
+"""Shapelet based transformers."""
+from tsml_eval._wip.transformations.collection.shapelet_based._shapelet_transform \
+    import (
+    RandomShapeletTransform,
+)
+
+__all__ = ["RandomShapeletTransform"]
