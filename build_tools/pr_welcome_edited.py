986d5a7ae949c4614d6e1e41bad93698cd20ddfa
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2024-07-19 11:53:23 +0100

    print

--- build_tools/pr_welcome_edited.py
+++ build_tools/pr_welcome_edited.py
@@ -58,6 +58,7 @@ with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
     print(f"branch={branch_name}", file=fh)  # noqa: T201
 
 if "- [x] Push an empty commit to re-run CI checks" in comment_body:
+    print("Pushing an empty commit to re-run CI checks")  # noqa: T201
     for comment in pr.get_comments():
         print(comment_user)  # noqa: T201
         print(comment.user.login)  # noqa: T201
@@ -80,5 +81,6 @@ if "- [x] Push an empty commit to re-run CI checks" in comment_body:
     with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
         print("empty_commit=true", file=fh)  # noqa: T201
 else:
+    print("Not pushing an empty commit")  # noqa: T201
     with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
         print("empty_commit=false", file=fh)  # noqa: T201
