3442f6211d3447f02fc2656f88713dc7b5bdf4a7
Chris Qiu <chuanhangqiu@outlook.com>
2024-11-18 16:48:25 +0000

    imb setting

--- tsml_eval/experiments/experiments.py
+++ tsml_eval/experiments/experiments.py
@@ -386,6 +386,7 @@ def load_and_run_classification_experiment(
     if imbalance_ratio:
         X_train, y_train = make_imbalance(X_train, y_train, sampling_ratio=imbalance_ratio,random_state=resample_id)
         X_test, y_test = make_imbalance(X_test, y_test, sampling_ratio=imbalance_ratio,random_state=resample_id)
+        print(f'X_train shape is {X_train.shape}, y_train shape is {y_train.shape}, X_test shape is {X_test.shape}, y_test shape is {y_test.shape}')
 
     if test_oversampling_methods:
         oversampler = getattr(SMOTE_FAMILY(), test_oversampling_methods)(seed=resample_id+2024)
