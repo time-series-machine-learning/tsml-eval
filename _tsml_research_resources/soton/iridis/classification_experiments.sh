cf337039fa2ad9de0a5ea7e619406d26034e9980
chuanhangqiu <cq2u24@soton.ac.uk>
2024-11-27 16:20:37 +0000

    imb change

--- _tsml_research_resources/soton/iridis/classification_experiments.sh
+++ _tsml_research_resources/soton/iridis/classification_experiments.sh
@@ -52,8 +52,8 @@ script_file_path="$local_path/tsml-eval/tsml_eval/experiments/classification_exp
 env_name="tsml-eval"
 
 # Classifiers to loop over. Must be seperated by a space
-# See list of potential classifiers in set_classifier
-classifiers_to_run="hc2"
+# See list of potential classifiers in set_classifier hc2,
+classifiers_to_run="multirockethydra"
 
 # You can add extra arguments here. See tsml_eval/utils/arguments.py parse_args
 # You will have to add any variable to the python call close to the bottom of the script
@@ -62,7 +62,7 @@ classifiers_to_run="hc2"
 # set the imbalance ration to create the imbalance data
 imbalance_ratio=19
 # Set to the oversampling methods you want to test \smote \adasyn
-toms="rose"
+toms=""
 results_dir="${results_dir}${toms}/"
 results_dir=$(echo "$results_dir" | sed 's#//*#/#g')
 out_dir="${out_dir}${toms}/"
