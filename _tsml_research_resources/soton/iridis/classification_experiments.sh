20914427ce708a188ff7edea9be0dec5a1e3c231
chuanhangqiu <cq2u24@soton.ac.uk>
2024-11-29 15:40:11 +0000

    imb experiment

--- _tsml_research_resources/soton/iridis/classification_experiments.sh
+++ _tsml_research_resources/soton/iridis/classification_experiments.sh
@@ -62,7 +62,7 @@ classifiers_to_run="multirockethydra"
 # set the imbalance ration to create the imbalance data
 imbalance_ratio=19
 # Set to the oversampling methods you want to test \smote \adasyn
-toms="adasyn"
+toms="rose"
 results_dir="${results_dir}${toms}/"
 results_dir=$(echo "$results_dir" | sed 's#//*#/#g')
 out_dir="${out_dir}${toms}/"
