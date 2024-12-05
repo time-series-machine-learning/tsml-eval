8546b067106d387564dce6f74412f347f82ffca8
MatthewMiddlehurst <pfm15hbu@gmail.com>
2024-12-05 22:56:20 +0000

    fixes

--- tsml_eval/testing/tests/__init__.py
+++ tsml_eval/testing/tests/__init__.py
@@ -4,11 +4,10 @@ import pkgutil
 
 import tsml_eval
 
-# collect all modules
-ALL_TSML_EVAL_MODULES = pkgutil.walk_packages(
-    tsml_eval.__path__, tsml_eval.__name__ + "."
-)
-ALL_TSML_EVAL_MODULES = [x[1] for x in ALL_TSML_EVAL_MODULES]
+# collect all modules except _wip
+ALL_TSML_EVAL_MODULES = [
+    x[1] for x in pkgutil.walk_packages(tsml_eval.__path__, tsml_eval.__name__ + ".")
+]
 ALL_TSML_EVAL_MODULES = [x for x in ALL_TSML_EVAL_MODULES if "_wip" not in x]
 
 ALL_TSML_EVAL_MODULES_NO_TESTS = [
