c1677cefa6095c020c099f880a2abe7ce83b2b61
Matthew Middlehurst <pfm15hbu@gmail.com>
2025-02-03 18:34:34 +0000

    Fixes and more unequal length junk (#326)
    * data transform option
    
    * scatter fix
    
    * fixes
    
    * tsml in extras
    
    * tsml bound
    
    * tsml bound
    
    * temp comment out tsml extras
    
    * comment xgboost
    
    * more deps
    
    * esig bound
    
    * esig bound
    
    * fixes
    
    * fixes
    
    * docs
    
    * notebook
    
    * unequal stuff
    
    * doc
    
    * more unequal
    
    * eval figs
    
    * fix
    
    * fixes

--- tsml_eval/_wip/unequal_length/tests/test_aeon.py
+++ tsml_eval/_wip/unequal_length/tests/test_aeon.py
@@ -1,11 +1,13 @@
 from aeon.testing.estimator_checking import parametrize_with_checks
 
 from tsml_eval._wip.unequal_length._arsenal import Arsenal
+from tsml_eval._wip.unequal_length._drcif import DrCIFClassifier
 from tsml_eval._wip.unequal_length._pad import Padder
 from tsml_eval._wip.unequal_length._rocket import RocketClassifier
 from tsml_eval._wip.unequal_length._stc import ShapeletTransformClassifier
+from tsml_eval._wip.unequal_length._tde import TemporalDictionaryEnsemble, IndividualTDE
 
 
-@parametrize_with_checks([Arsenal, ShapeletTransformClassifier, RocketClassifier, Padder])
+@parametrize_with_checks([Arsenal, ShapeletTransformClassifier, RocketClassifier, TemporalDictionaryEnsemble, IndividualTDE, DrCIFClassifier, Padder])
 def test_aeon_estimator(check):
     check()
