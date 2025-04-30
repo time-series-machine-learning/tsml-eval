1115998cbb5f38b425603fddb4da906db1035d42
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-04-30 12:26:08 +0100

    bigger ivc test data

--- tsml_eval/estimators/clustering/consensus/tests/test_ivc.py
+++ tsml_eval/estimators/clustering/consensus/tests/test_ivc.py
@@ -16,7 +16,7 @@ from tsml_eval.testing.testing_utils import _TEST_RESULTS_PATH
 @pytest.mark.parametrize("init", ["plus", "random", "aligned"])
 def test_ivc_init_methods(init):
     """Test IVC init methods."""
-    X, y = make_example_3d_numpy(n_labels=4)
+    X, y = make_example_3d_numpy(n_cases=30, n_timepoints=30, n_labels=4)
 
     ivc = IterativeVotingClustering(
         n_clusters=4,
