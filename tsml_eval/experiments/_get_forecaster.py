98ab9399d6ab5f6dc101a8b41acde7f40f439684
alexbanwell1 <31886108+alexbanwell1@users.noreply.github.com>
2025-05-13 04:48:11 +0000

    Automatic `pre-commit` fixes

--- tsml_eval/experiments/_get_forecaster.py
+++ tsml_eval/experiments/_get_forecaster.py
@@ -3,8 +3,8 @@
 __maintainer__ = ["MatthewMiddlehurst"]
 
 from aeon.forecasting import (
-    AutoETSForecaster,
     ARIMAForecaster,
+    AutoETSForecaster,
     DummyForecaster,
     ETSForecaster,
     NaiveForecaster,
