669ad9f521c4198e6de10dad6c9eab503f44c91c
Alex Banwell <arb1g19@soton.ac.uk>
2025-06-04 21:35:44 +0100

    Update _get_forecaster for use with all forecasting algorithms found in branch ajb/ets

--- tsml_eval/experiments/_get_forecaster.py
+++ tsml_eval/experiments/_get_forecaster.py
@@ -5,26 +5,24 @@ __maintainer__ = ["MatthewMiddlehurst"]
 from aeon.forecasting import (
     AutoSARIMAForecaster,
     AutoARIMAForecaster,
-    # AutoETSForecaster,
-    DummyForecaster,
+    AutoETSForecaster,
     ETSForecaster,
     NaiveForecaster,
 )
-# from aeon.forecasting._sktime_autoets import SktimeAutoETSForecaster
-# from aeon.forecasting._statsforecast_autoets import StatsForecastAutoETSForecaster
+from aeon.forecasting._sktime_autoets import SktimeAutoETSForecaster
+from aeon.forecasting._statsforecast_autoets import StatsForecastAutoETSForecaster
 
 from tsml_eval.utils.functions import str_in_nested_list
 
 stats_forecasters = [
     ["etsforecaster", "ets"],
-    # ["autoetsforecaster", "autoets"],
+    ["autoetsforecaster", "autoets"],
     ["autosarima", "sarima"],
     ["autoarima", "arima"],
-    # "sktimeets",
-    # "statsforecastets",
+    "sktimeets",
+    "statsforecastets",
 ]
 other_forecasters = [
-    ["dummyforecaster", "dummy"],
     ["naiveforecaster", "naive"],
 ]
 
@@ -69,12 +67,12 @@ def get_forecaster_by_name(forecaster_name, random_state=None, n_jobs=1, **kwarg
 def _set_forecaster_stats(f, random_state, n_jobs, kwargs):
     if f == "etsforecaster" or f == "ets":
         return ETSForecaster(**kwargs)
-    # if f == "autoetsforecaster" or f == "autoets":
-    #     return AutoETSForecaster(**kwargs)
-    # if f == "sktimeets":
-    #     return SktimeAutoETSForecaster(**kwargs)
-    # if f == "statsforecastets":
-    #     return StatsForecastAutoETSForecaster(**kwargs)
+    if f == "autoetsforecaster" or f == "autoets":
+        return AutoETSForecaster(**kwargs)
+    if f == "sktimeets":
+        return SktimeAutoETSForecaster(**kwargs)
+    if f == "statsforecastets":
+        return StatsForecastAutoETSForecaster(**kwargs)
     if f == "autosarima" or f == "sarima":
         return AutoSARIMAForecaster(**kwargs)
     if f == "autoarima" or f == "arima":
@@ -82,7 +80,5 @@ def _set_forecaster_stats(f, random_state, n_jobs, kwargs):
 
 
 def _set_forecaster_other(f, random_state, n_jobs, kwargs):
-    if f == "dummyforecaster" or f == "dummy":
-        return DummyForecaster(**kwargs)
     if f == "naiveforecaster" or f == "naive":
         return NaiveForecaster(**kwargs)
