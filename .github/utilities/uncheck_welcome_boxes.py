01a23ed204a0e8727a05e2708e98b989a02933a7
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-08-11 21:35:42 +0100

    proper box unchecking

--- .github/utilities/uncheck_welcome_boxes.py
+++ .github/utilities/uncheck_welcome_boxes.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import sys
 
 from github import Github
 
@@ -11,8 +12,17 @@ repo = context_dict["repository"]
 g = Github(os.getenv("GITHUB_TOKEN"))
 repo = g.get_repo(repo)
 issue_number = context_dict["event"]["issue"]["number"]
-pr = repo.get_issue(number=issue_number).as_pull_request()
+issue = repo.get_issue(number=issue_number)
 comment_body = context_dict["event"]["comment"]["body"]
+comment_user = context_dict["event"]["comment"]["user"]["login"]
+
+if (
+    issue.pull_request is None
+    or "## Thank you for contributing to `tsml-eval`" not in comment_body
+):
+    sys.exit(0)
+
+pr = issue.as_pull_request()
 comment = pr.get_issue_comment(context_dict["event"]["comment"]["id"])
 
 if "- [x] Push an empty commit to re-run CI checks" in comment_body:
