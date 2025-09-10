675f288c4da9e7abd1428270478c327e8d7e393f
MatthewMiddlehurst <pfm15hbu@gmail.com>
2025-09-10 21:32:51 +0100

    tsml update

--- tsml_eval/estimators/__init__.py
+++ tsml_eval/estimators/__init__.py
@@ -1,13 +1,1 @@
 """Estimators that are not present in tsml or aeon."""
-
-__all__ = [
-    "SklearnToTsmlClassifier",
-    "SklearnToTsmlClusterer",
-    "SklearnToTsmlRegressor",
-]
-
-from tsml_eval.estimators.classification._sklearn_classifier import (
-    SklearnToTsmlClassifier,
-)
-from tsml_eval.estimators.clustering._sklearn_clusterer import SklearnToTsmlClusterer
-from tsml_eval.estimators.regression._sklearn_regressor import SklearnToTsmlRegressor
--- tsml_eval/estimators/classification/__init__.py
+++ tsml_eval/estimators/classification/__init__.py
@@ -1,9 +1,1 @@
 """Classification estimators."""
-
-__all__ = [
-    "SklearnToTsmlClassifier",
-]
-
-from tsml_eval.estimators.classification._sklearn_classifier import (
-    SklearnToTsmlClassifier,
-)
