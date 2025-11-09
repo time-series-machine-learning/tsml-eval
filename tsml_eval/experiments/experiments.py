21fa7400d7c59d4706fa8ad441a13b9a8558256c
Alex Banwell <alexbanwell1@gmail.com>
2025-11-09 13:21:08 +0000

    implement get experimental data for forecasting

--- tsml_eval/experiments/experiments.py
+++ tsml_eval/experiments/experiments.py
@@ -657,8 +657,15 @@ class ForecastingExperiment(Experiment):
         pass
 
     def load_experimental_data(self):
-        # TODO IMPLEMENT
-        return super().load_experimental_data()
+        train = pd.read_csv(
+            f"{self.problem_path}/{self.dataset_name}/{self.dataset_name}_TRAIN.csv", index_col=0
+        ).squeeze("columns")
+        train = train.astype(float).to_numpy()
+        test = pd.read_csv(
+            f"{self.problem_path}/{self.dataset_name}/{self.dataset_name}_TEST.csv", index_col=0
+        ).squeeze("columns")
+        test = test.astype(float).to_numpy()
+        return train, None, test, None
 
     def generate_test_preds(self, x_test, y_test):
         # TODO Implement this and train_preds properly
