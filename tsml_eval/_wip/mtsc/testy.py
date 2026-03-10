6fbeeca8d449e9051c94132fda84acc9a4e6dd9f
Tony Bagnall <ajb@uea.ac.uk>
2026-03-10 13:37:09 +0000

    paths

--- tsml_eval/_wip/mtsc/testy.py
+++ tsml_eval/_wip/mtsc/testy.py
@@ -1,7 +1,7 @@
 print("Hello world, up the arsenal")
 from aeon.datasets import load_classification
 from tsml_eval.experiments import load_and_run_classification_experiment
-
+from pathlib import Path
 
 path = str(Path("~/Data").expanduser())
 results = str(Path("~/Results").expanduser())
