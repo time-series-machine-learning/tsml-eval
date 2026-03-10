cb07fd0eabe619b1b4eace048be797a37399eed8
Tony Bagnall <ajb@uea.ac.uk>
2026-03-10 13:38:05 +0000

    paths

--- tsml_eval/_wip/mtsc/testy.py
+++ tsml_eval/_wip/mtsc/testy.py
@@ -1,12 +1,13 @@
-print("Hello world, up the arsenal")
-from aeon.datasets import load_classification
-from tsml_eval.experiments import load_and_run_classification_experiment
 from pathlib import Path
+from tsml_eval.experiments import load_and_run_classification_experiment
 
 path = str(Path("~/Data").expanduser())
 results = str(Path("~/Results").expanduser())
 data = "Chinatown"
 
-load_and_run_classification_experiment(problem_path=path,results_path=results,
-                                       dataset=data, classifier="Arsenal")
-
+load_and_run_classification_experiment(
+    problem_path=path,
+    results_path=results,
+    dataset=data,
+    classifier="Arsenal",
+)
