52eef7aa31f547b562e96fa75fcad33204c4a8f1
Tony Bagnall <ajb@uea.ac.uk>
2026-03-10 12:55:18 +0000

    experiment

--- tsml_eval/_wip/mtsc/testy.py
+++ tsml_eval/_wip/mtsc/testy.py
@@ -1,1 +1,13 @@
+from local.classification import trainy
+
 print("Hello world, up the arsenal")
+from aeon.datasets import load_classification
+from tsml_eval.experiments import load_and_run_classification_experiment
+
+path = "~\\Data\\"
+results = "~\\Results\\"
+data = "Chinatown"
+
+load_and_run_classification_experiment(problem_path=path,results_path=results,
+                                       dataset=data, classifier="Arsenal")
+
