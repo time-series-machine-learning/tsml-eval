232b7cdff24fc2a91d9fe7db24c6ca9262cecfa3
Tony Bagnall <ajb@uea.ac.uk>
2026-03-23 16:02:44 +0000

    Revert "Try loki"
    This reverts commit 9414ce0d9da692c6eeca5e099375ee3eb6cc899c.

--- tsml_eval/experiments/_get_classifier.py
+++ tsml_eval/experiments/_get_classifier.py
@@ -710,7 +710,6 @@ def _set_classifier_interval_based(
             random_state=random_state,
             n_jobs=n_jobs,
             time_limit_in_minutes=fit_contract,
-            parallel_backend="loky",
             **kwargs,
         )
     elif c == "drcif" or c == "drcifclassifier":
