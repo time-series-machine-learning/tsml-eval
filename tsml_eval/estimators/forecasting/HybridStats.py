7a283c1d2630c7e155efb300f7c5813707cf467e
Alex Banwell <alexbanwell1@gmail.com>
2026-05-07 17:22:15 +0100

    Change weight function

--- tsml_eval/estimators/forecasting/HybridStats.py
+++ tsml_eval/estimators/forecasting/HybridStats.py
@@ -83,7 +83,7 @@ class AverageStatsAIC(BaseForecaster):
         ])
 
         # convert AIC → weights (lower AIC = higher weight)
-        weights = np.exp(-(aics - np.min(aics)) / 50)
+        weights = np.exp(-(aics - np.min(aics)) / 500)
         weights /= weights.sum()
         print(weights)
         print(forecasts)
